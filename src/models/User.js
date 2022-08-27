aab58b3ded1683e9f6a6d7c03562ba5be3b98e09
Joao Barros <joao_barross@hotmail.com>
2022-08-27 02:48:06 -0300

    Add Role to the User and format the doc with VScod

--- src/models/User.js
+++ src/models/User.js
@@ -2,53 +2,58 @@ const Sequelize = require('sequelize');
 const db = require('./db');
 
 const User = db.define('users', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true,
-    },
-    name: {
-        type: Sequelize.STRING, 
-        allowNull: false,
-        validate:{
-            notEmpty: true,
-        }
-    },
-    age: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        validate:{
-            notEmpty: true,
-            isInt: true
-        }
-    },
-    cpf: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        validate:{
-            notEmpty: true,
-        }
-    },
-    email: {
-        type: Sequelize.STRING, 
-        allowNull: false,
-        validate:{
-            notEmpty: true,
-            isEmail: true
-        }
-    },
-    password: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        validate:{
-            notEmpty: true,
-        }
-    },
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    }
+  },
+  role: {
+    type: Sequelize.ENUM('admin', 'user'),
+    allowNull: false,
+    defaultValue: 'user',
+  },
+  age: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      isInt: true
+    }
+  },
+  cpf: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    }
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      isEmail: true
+    }
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    }
+  },
 });
 
 //Criar a tabela
-//User.sync()
+User.sync()
 //Verifica se a tabela ja existe, e se sim, so a altera.
 User.sync({ alter: true })
 
