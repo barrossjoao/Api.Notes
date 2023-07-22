ba4eb4adc5dd9a6dabf320d936169ba716f1c82e
Joao Barros <joao_barross@hotmail.com>
2023-07-22 00:39:20 -0300

    correcoes

--- seeders/20230722032018-notes.js
+++ seeders/20230722032018-notes.js
@@ -3,14 +3,14 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('notes', [
       {
-        userId: 6, 
+        userId: 1, 
         title: 'Nota do João',
         content: 'Conteúdo da nota do João.',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        userId: 7, 
+        userId: 2, 
         title: 'Nota da Maria',
         content: 'Conteúdo da nota da Maria.',
         createdAt: new Date(),
